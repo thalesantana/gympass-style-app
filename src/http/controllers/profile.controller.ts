a9878e1c336b50e260279285b7a8a1a92566d1ff
Thales Santana <thales.c.santana@outlook.com>
2024-08-12 23:14:21 -0300

    add jwt validation to a middleware

--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -1,12 +1,20 @@
+import { makeGetUserProfileUseCase } from '@/services/factories/make-get-user-profile-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile ( 
   request: FastifyRequest, 
   reply: FastifyReply 
 ) {
-  await request.jwtVerify()
-
+  const getUseProfile = makeGetUserProfileUseCase()
   
+  const { user } = await getUseProfile.execute({
+    userId: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined
+    }
+  })
 }
