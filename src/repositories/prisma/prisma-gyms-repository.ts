6b91b356df6ddcd4a6d19c974973ebcea13a68b4
Thales Santana <thales.c.santana@outlook.com>
2024-08-12 21:19:24 -0300

    added gyms repository

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,43 @@
+import { Prisma, Gym } from '@prisma/client';
+import { findManyNearbyParams, GymsRepository } from '../gyms-repository.js'
+import { prisma } from '@/lib/prisma.js';
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym;
+  }
+  async searchMany(query: string, page: number) {
+    const gym = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      skip: (page - 1) * 20,
+      take: 20,
+    })
+
+    return gym;
+  }
+  async findManyNearby({latitude, longitude}: findManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    ` 
+    return gyms;
+  }
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data
+    })
+
+    return gym;
+  }
+
+}
